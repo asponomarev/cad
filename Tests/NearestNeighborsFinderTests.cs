be38394ceeb27107a89bbe3e08f53e0d3988ed04
n.borodin <n.borodin@integriks.ru>
2023-04-15 11:23:01 +0300

    wrote tests

---
+++ Tests/NearestNeighborsFinderTests.cs
@@ -0,0 +1,106 @@
+﻿using DSS;
+using Moq.AutoMock;
+using UhlnocsServer.Models.Properties.Parameters;
+using UhlnocsServer.Models.Properties.Parameters.Values;
+
+namespace Tests
+{
+    public class NearestNeighborsFinderTests
+    {
+        [Test]
+        public void FoundBiggerAndSmallerNeighbors()
+        {
+            var knownParameters = new List<ParameterValue>()
+            {
+                new IntParameterValue("Parameter1", 2),
+                new IntParameterValue("Parameter2", 2),
+                new DoubleParameterValue("Parameter4", 2.2),
+                new BoolParameterValue("Parameter7", false),
+                new StringParameterValue("Parameter8", "Parameter8Parameter8")
+            };
+            var expectedParameters = new List<List<ParameterValue>>()
+            {
+                new List<ParameterValue>()
+                {
+                    new IntParameterValue("Parameter1", 3),
+                    new IntParameterValue("Parameter2", 3),
+                    new IntParameterValue("Parameter3", 3),
+                    new DoubleParameterValue("Parameter4", 3.3),
+                    new DoubleParameterValue("Parameter5", 3.3),
+                    new BoolParameterValue("Parameter6", true),
+                    new BoolParameterValue("Parameter7", false),
+                    new StringParameterValue("Parameter8", "Parameter8Parameter8")
+                },
+                new List<ParameterValue>()
+                {
+                    new IntParameterValue("Parameter1", 1),
+                    new IntParameterValue("Parameter2", 1),
+                    new IntParameterValue("Parameter3", 1),
+                    new DoubleParameterValue("Parameter4", 1.1),
+                    new DoubleParameterValue("Parameter5", 1.1),
+                    new BoolParameterValue("Parameter6", true),
+                    new BoolParameterValue("Parameter7", false),
+                    new StringParameterValue("Parameter8", "Parameter8Parameter8")
+                },
+            };
+            var autoMocker = new AutoMocker();
+            var nearestNeighborsFinder = autoMocker.CreateInstance<NearestNeighborsFinder>();
+            var resultParameters = nearestNeighborsFinder.Find(ClassCreator.GetListsParameters(), knownParameters, 2);
+            Assert.IsNotNull(resultParameters);
+            Assert.AreEqual(expectedParameters.Count, resultParameters.Count);
+            CompareParametersLists(expectedParameters, resultParameters);
+        }
+
+        [Test]
+        public void NeighborsFoundIgnoringBoolAndStringFields()
+        {
+            var knownParameters = new List<ParameterValue>()
+            {
+                new IntParameterValue("Parameter1", 2),
+                new IntParameterValue("Parameter2", 2),
+                new DoubleParameterValue("Parameter4", 2.2),
+                new BoolParameterValue("Parameter7", true),
+                new StringParameterValue("Parameter8", "ParameterTest")
+            };
+            var expectedParameters = new List<List<ParameterValue>>()
+            {
+                new List<ParameterValue>()
+                {
+                    new IntParameterValue("Parameter1", 3),
+                    new IntParameterValue("Parameter2", 3),
+                    new IntParameterValue("Parameter3", 3),
+                    new DoubleParameterValue("Parameter4", 3.3),
+                    new DoubleParameterValue("Parameter5", 3.3),
+                    new BoolParameterValue("Parameter6", true),
+                    new BoolParameterValue("Parameter7", false),
+                    new StringParameterValue("Parameter8", "Parameter8Parameter8")
+                },
+                new List<ParameterValue>()
+                {
+                    new IntParameterValue("Parameter1", 1),
+                    new IntParameterValue("Parameter2", 1),
+                    new IntParameterValue("Parameter3", 1),
+                    new DoubleParameterValue("Parameter4", 1.1),
+                    new DoubleParameterValue("Parameter5", 1.1),
+                    new BoolParameterValue("Parameter6", true),
+                    new BoolParameterValue("Parameter7", false),
+                    new StringParameterValue("Parameter8", "Parameter8Parameter8")
+                },
+            };
+            var autoMocker = new AutoMocker();
+            var nearestNeighborsFinder = autoMocker.CreateInstance<NearestNeighborsFinder>();
+            var resultParameters = nearestNeighborsFinder.Find(ClassCreator.GetListsParameters(), knownParameters, 2);
+            Assert.IsNotNull(resultParameters);
+            Assert.AreEqual(expectedParameters.Count, resultParameters.Count);
+            CompareParametersLists(expectedParameters, resultParameters);
+        }
+
+        private void CompareParametersLists(List<List<ParameterValue>> firstList, List<List<ParameterValue>> secondList)
+        {
+            for (var i = 0; i < firstList.Count; i++)
+            {
+                Assert.IsTrue(ResultsComparator.IsEquals(firstList[i], secondList[i]));
+            }
+        }
+    }
+}
---
+++ Tests/ResultsComparator.cs
@@ -0,0 +1,56 @@
+﻿using UhlnocsServer.Models.Properties.Characteristics;
+using UhlnocsServer.Models.Properties.Parameters;
+using UhlnocsServer.Models.Properties.Parameters.Values;
+
+namespace Tests
+{
+    public static class ResultsComparator
+    {
+        public static bool IsEquals(List<ParameterValue> firstParams, List<ParameterValue> secondParams)
+        {
+            var isEquals = false;
+            foreach (var param in firstParams)
+            {
+                if (param is IntParameterValue firstInt && secondParams.FirstOrDefault(p => string.Equals(p.Id, param.Id)) is IntParameterValue secondInt)
+                {
+                    isEquals = string.Equals(firstInt.Id, secondInt.Id)
+                        && firstInt.Value == secondInt.Value;
+                }
+                else if (param is DoubleParameterValue firstDouble && secondParams.FirstOrDefault(p => string.Equals(p.Id, param.Id)) is DoubleParameterValue secondDouble)
+                {
+                    isEquals = string.Equals(firstDouble.Id, secondDouble.Id)
+                        && firstDouble.Value == secondDouble.Value;
+                }
+                else if (param is BoolParameterValue firstBool && secondParams.FirstOrDefault(p => string.Equals(p.Id, param.Id)) is BoolParameterValue secondBool)
+                {
+                    isEquals = string.Equals(firstBool.Id, secondBool.Id)
+                        && firstBool.Value == secondBool.Value;
+                }
+                else if (param is StringParameterValue firstString && secondParams.FirstOrDefault(p => string.Equals(p.Id, param.Id)) is StringParameterValue secondString)
+                {
+                    isEquals = string.Equals(firstString.Id, secondString.Id)
+                        && string.Equals(firstString.Value, secondString.Value);
+                }
+
+                if (!isEquals)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static bool IsEquals(List<CharacteristicWithModel> firstCharacteristics, List<CharacteristicWithModel> secondCharacteristics)
+        {
+            for (var i = 1; i < firstCharacteristics.Count; i++)
+            {
+                if (!(string.Equals(firstCharacteristics[i].Id, secondCharacteristics[i].Id) 
+                    && string.Equals(firstCharacteristics[i].Model, secondCharacteristics[i].Model)))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
