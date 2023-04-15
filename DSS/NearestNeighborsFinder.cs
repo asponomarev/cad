97ea0c4de45a1c804c4b0c0a29efeb3f0235b57a
n.borodin <n.borodin@integriks.ru>
2023-04-15 11:22:16 +0300

    implementation interfaces

---
+++ DSS/NearestNeighborsFinder.cs
@@ -0,0 +1,45 @@
+﻿using UhlnocsServer.Models.Properties.Parameters;
+using UhlnocsServer.Models.Properties.Parameters.Values;
+
+namespace DSS
+{
+    public class NearestNeighborsFinder : INearestNeighborsFinder
+    {
+        public List<List<ParameterValue>> Find(List<List<ParameterValue>> parametersList, List<ParameterValue> knownParameters, int k)
+        {
+            {
+                var distances = new List<double>();
+                for (int i = 0; i < parametersList.Count; i++)
+                {
+                    double distance = 0;
+                    foreach (var knownParameter in knownParameters)
+                    {
+                        if (knownParameter is IntParameterValue knownIntParameterValue && parametersList[i].FirstOrDefault(p => string.Equals(p.Id, knownParameter.Id)) is IntParameterValue intParameterValue)
+                        {
+                            // Считаем расстояние между элементами списков.
+                            double diff = Convert.ToDouble(intParameterValue.Value) - Convert.ToDouble(knownIntParameterValue.Value);
+                            distance += diff * diff;
+                        }
+                        else if (knownParameter is DoubleParameterValue knownDoubleParameterValue && parametersList[i].FirstOrDefault(p => string.Equals(p.Id, knownParameter.Id)) is DoubleParameterValue doubleParameterValue)
+                        {
+                            // Считаем расстояние между элементами списков.
+                            double diff = doubleParameterValue.Value - knownDoubleParameterValue.Value;
+                            distance += diff * diff;
+                        }
+                    }
+                    distances.Add(distance);
+                }
+
+                // Сортируем списки по возрастанию расстояния.
+                var sortedLists = parametersList
+                    .Select((list, index) => new { List = list, Index = index })
+                    .OrderBy(pair => distances[pair.Index])
+                    .Select(pair => pair.List)
+                    .ToList();
+
+                // Выбираем первые k списков, которые будут являться ближайшими соседями.
+                return sortedLists.Take(k).ToList();
+            }
+        }
+    }
+}
---
+++ DSS/Wrappers/ServerWrapper.cs
@@ -0,0 +1,25 @@
+﻿using UhlnocsServer.Calculations;
+using UhlnocsServer.Models;
+using UhlnocsServer.Models.Properties.Characteristics;
+using UhlnocsServer.Services;
+
+namespace DSS.Wrappers
+{
+    public class ServerWrapper : IServerWrapper
+    {
+        public IList<LaunchConfiguration> GetLaunchConfigurations()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ModelConfiguration GetModelConfiguration(string modelId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dictionary<string, CharacteristicWithModels> GetCharacteristicsWithModels()
+        {
+            return ModelService.CharacteristicsWithModels;
+        }
+    }
+}
