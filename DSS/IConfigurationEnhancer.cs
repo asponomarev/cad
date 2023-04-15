70419104c28622b786fb03c078eadf09ccc06e16
n.borodin <n.borodin@integriks.ru>
2023-04-15 11:21:08 +0300

    create dss interfaces

---
+++ DSS/IConfigurationEnhancer.cs
@@ -0,0 +1,9 @@
+﻿using UhlnocsServer.Calculations;
+
+namespace DSS
+{
+    public interface IConfigurationEnhancer
+    {
+        public LaunchConfiguration GetModifiedLaunchConfiguration(LaunchConfiguration launchConfiguration);
+    }
+}
---
+++ DSS/IParameterFinder.cs
@@ -0,0 +1,14 @@
+﻿using UhlnocsServer.Calculations;
+using UhlnocsServer.Models.Properties.Parameters;
+
+namespace DSS
+{
+    public interface IParameterFinder
+    {
+        public IList<ParameterValue> GetMatching(IList<ParameterValue> knownParameters, string modelId);
+
+        public IList<ParameterValue> GetNearest(IList<ParameterValue> knownParameters, string modelId, double searchAccuracy);
+
+        public IList<ParameterValue> GetDefault(IList<ParameterValue> knownParameters, IList<ParameterInfo> parameterInfos);
+    }
+}
