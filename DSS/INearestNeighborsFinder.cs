009e80656e733c5c77bf4c601d3f575ce91c819d
n.borodin <n.borodin@integriks.ru>
2023-04-15 11:21:08 +0300

    create dss interfaces

---
+++ DSS/INearestNeighborsFinder.cs
@@ -0,0 +1,9 @@
+﻿using UhlnocsServer.Models.Properties.Parameters;
+
+namespace DSS
+{
+    public interface INearestNeighborsFinder
+    {
+        public List<List<ParameterValue>> Find(List<List<ParameterValue>> parametersList, List<ParameterValue> knownParameters, int k);
+    }
+}
