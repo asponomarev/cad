d05ea0815cc12f2428ccdc7da227010f701fe6fc
Пономарев Александр Сергеевич <asponomarev@edu.hse.ru>
2023-04-28 21:03:30 +0200

    Fixed problems caused by usage of db context by multiple threads

--- UhlnocsServer/Database/Repository.cs
+++ UhlnocsServer/Database/Repository.cs
@@ -2,47 +2,50 @@
 {
     public class Repository<T> : IRepository<T> where T : class
     {
-        private ApplicationDatabaseContext AppDbContext;
-
-        public Repository(ApplicationDatabaseContext appDbContext)
+        public Repository()
         {
-            AppDbContext = appDbContext;
+
         }
 
         public async Task Create(T entity)
         {
-            var dbSet = AppDbContext.Set<T>();
+            ApplicationDatabaseContext appDbContext = new();
+            var dbSet = appDbContext.Set<T>();
             dbSet.Add(entity);
-            await AppDbContext.SaveChangesAsync();
+            await appDbContext.SaveChangesAsync();
         }
 
         public IQueryable<T> Get()
         {
-            var dbSet = AppDbContext.Set<T>();
+            ApplicationDatabaseContext appDbContext = new();
+            var dbSet = appDbContext.Set<T>();
             return dbSet.AsQueryable();
         }
 
         public async Task<T?> GetById(string id)
         {
-            var dbSet = AppDbContext.Set<T>();
+            ApplicationDatabaseContext appDbContext = new();
+            var dbSet = appDbContext.Set<T>();
             return await dbSet.FindAsync(id);
         }
 
         public async Task Update(T entity)
         {
-            var dbSet = AppDbContext.Set<T>();
+            ApplicationDatabaseContext appDbContext = new();
+            var dbSet = appDbContext.Set<T>();
             dbSet.Update(entity);
-            await AppDbContext.SaveChangesAsync();
+            await appDbContext.SaveChangesAsync();
         }
 
         public async Task Delete(string id)
         {
+            ApplicationDatabaseContext appDbContext = new();
             var entity = await GetById(id);
             if (entity != null)
             {
-                var dbSet = AppDbContext.Set<T>();
+                var dbSet = appDbContext.Set<T>();
                 dbSet.Remove(entity);
-                await AppDbContext.SaveChangesAsync();
+                await appDbContext.SaveChangesAsync();
             }
         }
     }
