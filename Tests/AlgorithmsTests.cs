25992f2df63d0436553069855c5d4be7ae624931
tvtarzhanov <tvtarzhanov@edu.hse.ru>
2023-05-05 18:29:55 +0300

    fix-tests-of-algorithms

--- Tests/AlgorithmsTests.cs
+++ Tests/AlgorithmsTests.cs
@@ -31,17 +31,20 @@ namespace Tests
             {
                 if (i == 0)
                 {
-                    calculationParameters = constantStep.MakeCalculationParameters(parameters, "Parameter3", i, PropertyValueType.String, variableParameter, parameterInfo);
+                    calculationParameters = constantStep.MakeCalculationParameters(parameters, "Parameter3", i, 
+                                                    PropertyValueType.String, variableParameter, parameterInfo);
                     calculatedParameter = ParameterValue.GetFromListById(calculationParameters, "Parameter3");
                     string stringValue = ((StringParameterValue)calculatedParameter).Value;
                     Assert.That(stringValue, Is.EqualTo("First")); // проверка, что рассчитанный строковый параметр соответствует ожиданию
 
-                    calculationParameters = constantStep.MakeCalculationParameters(parameters, "Parameter4", i, PropertyValueType.Bool, variableParameter, parameterInfo);
+                    calculationParameters = constantStep.MakeCalculationParameters(parameters, "Parameter4", i, 
+                                                      PropertyValueType.Bool, variableParameter, parameterInfo);
                     calculatedParameter = ParameterValue.GetFromListById(calculationParameters, "Parameter4");
                     bool boolValue = ((BoolParameterValue)calculatedParameter).Value;
                     Assert.That(boolValue, Is.EqualTo(true)); // проверка, что рассчитанный логический параметр соответствует ожиданию
                 }
-                calculationParameters = constantStep.MakeCalculationParameters(parameters, "Parameter1", i, PropertyValueType.Int, variableParameter, parameterInfo);
+                calculationParameters = constantStep.MakeCalculationParameters(parameters, "Parameter1", i, 
+                                                        PropertyValueType.Int, variableParameter, parameterInfo);
 
             }
             calculatedParameter = ParameterValue.GetFromListById(calculationParameters, "Parameter1");
@@ -73,8 +76,11 @@ namespace Tests
                 }
             }
             while (Status == AlgorithmStatus.Calculating);
-            Assert.That(Status, Is.EqualTo(AlgorithmStatus.ReachedMaxIteration)); // проверка, что статус алгоритма поменялся
-            Assert.That(iteration, Is.EqualTo(1)); // проверка, что цикл закончился на 1 итерации, так как maxIterations было задано 1
+            Assert.Multiple(() =>
+            {
+                Assert.That(Status, Is.EqualTo(AlgorithmStatus.ReachedMaxIteration)); // проверка, что статус алгоритма поменялся
+                Assert.That(iteration, Is.EqualTo(1)); // проверка, что цикл закончился на 1 итерации
+            });
         }
 
         [Test]
@@ -86,7 +92,6 @@ namespace Tests
             double maxRate = 10;
             BinarySearch binarySearch = new BinarySearch(variableParameterId, "Characteristic1", iterations, maxRate);
             binarySearch.FirstValue = 1;
-            double throughputCharacteristicValue;
             AlgorithmStatus Status;
 
             for (int i = 0; i < 3; ++i)
@@ -97,29 +102,24 @@ namespace Tests
                     ParameterValue variableParameter = ParameterValue.GetFromListById(calculationParameters, variableParameterId);
                     double CalculatedValue = ((DoubleParameterValue)variableParameter).Value;
                     Assert.That(CalculatedValue, Is.EqualTo(1)); // проверка, что на 0 итерации возвращается левая граница
-                    throughputCharacteristicValue = 0;
-                    Status = binarySearch.MoveBorder(throughputCharacteristicValue, i);
-                    Assert.That(Status, Is.EqualTo(AlgorithmStatus.FirstPointIsBad)); // проверка, что находится "первая точка плохая"
+                    Status = binarySearch.MoveBorder(0, i);
+                    Assert.That(Status, Is.EqualTo(AlgorithmStatus.FirstPointIsBad)); // проверка, что точка насыщения до целевого отрезка
                 }
                 else if (i == 1)
                 {
                     ParameterValue variableParameter = ParameterValue.GetFromListById(calculationParameters, variableParameterId);
                     double CalculatedValue = ((DoubleParameterValue)variableParameter).Value;
                     Assert.That(CalculatedValue, Is.EqualTo(10)); // проверка, что на 1 итерации возвращается правая граница
-                    throughputCharacteristicValue = 10;
-                    Status = binarySearch.MoveBorder(throughputCharacteristicValue, i);
-                    Assert.That(Status, Is.EqualTo(AlgorithmStatus.LastPointIsGood)); // проверка, что находится "последняя точка хорошая"
+                    Status = binarySearch.MoveBorder(10, i);
+                    Assert.That(Status, Is.EqualTo(AlgorithmStatus.LastPointIsGood)); // проверка, что точка насыщения после целевого отрезка
                 }
                 else
                 {
-                    throughputCharacteristicValue = 5.5;
-                    _ = binarySearch.MoveBorder(throughputCharacteristicValue, i);
-                    Assert.That(binarySearch.FirstValue, Is.EqualTo(5.5)); // проверка, что найденная throughput "хорошая", поменялась левая граница
+                    _ = binarySearch.MoveBorder(5.5, i);
+                    Assert.That(binarySearch.FirstValue, Is.EqualTo(5.5)); // проверка, что поменялась левая граница
 
-                    throughputCharacteristicValue = 3;
-                    _ = binarySearch.MoveBorder(throughputCharacteristicValue, i);
-                    Assert.That(binarySearch.LastValue, Is.EqualTo(5.5)); // проверка, что найденная throughput "плохая", поменялась правая граница
-
+                    _ = binarySearch.MoveBorder(3, i);
+                    Assert.That(binarySearch.LastValue, Is.EqualTo(5.5)); // проверка, что поменялась правая граница
                 }
             }
         }
@@ -132,7 +132,6 @@ namespace Tests
             double maxRate = 50;
             SmartBinarySearch smartBinarySearch = new SmartBinarySearch(variableParameterId, "Characteristic1", maxRate);
             smartBinarySearch.FirstValue = 1;
-            double throughputCharacteristicValue;
             AlgorithmStatus Status = AlgorithmStatus.Calculating;
             int iteration = 0;
             do
@@ -141,38 +140,32 @@ namespace Tests
 
                 if (iteration == 2)
                 {
-                    Assert.That(smartBinarySearch.FirstChangedBorder, Is.EqualTo(null)); // проверка, что первые две итерации не повлияли на поля класса
-                    throughputCharacteristicValue = 0;
-                    Status = smartBinarySearch.MoveBorder(throughputCharacteristicValue, iteration);
+                    Assert.That(smartBinarySearch.FirstChangedBorder, Is.EqualTo(null)); // проверка, что первые две итерации
+                                                                                         // не повлияли на поля класса
+                    Status = smartBinarySearch.MoveBorder(0, iteration);
                     Assert.Multiple(() =>
                     {
                         Assert.That(smartBinarySearch.FirstChangedBorder, Is.EqualTo("Right")); // проверка, что первой поменялась правая граница
                         Assert.That(smartBinarySearch.BothBordersChanged, Is.EqualTo(false)); // проверка, что еще не были изменены обе границы
                     });
                 }
-
                 if (iteration == 3)
                 {
-                    throughputCharacteristicValue = 13;
-                    Status = smartBinarySearch.MoveBorder(throughputCharacteristicValue, iteration);
+                    Status = smartBinarySearch.MoveBorder(13, iteration);
                     Assert.That(smartBinarySearch.BothBordersChanged, Is.EqualTo(true)); // проверка, что были изменены обе границы
                 }
-
                 if (iteration == 4)
                 {
-                    throughputCharacteristicValue = 0;
-                    Status = smartBinarySearch.MoveBorder(throughputCharacteristicValue, iteration);
+                    Status = smartBinarySearch.MoveBorder(0, iteration);
                     Assert.That(smartBinarySearch.ReachedSaturationPoint, Is.EqualTo(true)); // проверка, что точка насыщения достигнута
                 }
-
                 if (iteration == 5)
                 {
-                    throughputCharacteristicValue = 16;
-                    Status = smartBinarySearch.MoveBorder(throughputCharacteristicValue, iteration);
+                    Status = smartBinarySearch.MoveBorder(16, iteration);
                     Assert.That(Status, Is.EqualTo(AlgorithmStatus.FoundSaturationPoint)); // проверка, что статус алгоритма поменялся
                     ParameterValue variableParameter = ParameterValue.GetFromListById(calculationParameters, variableParameterId);
                     double CalculatedValue = ((DoubleParameterValue)variableParameter).Value;
-                    Assert.That(CalculatedValue, Is.EqualTo(16.3125d)); // проверка, что найдена искомая точка насыщения
+                    Assert.That(CalculatedValue, Is.EqualTo(16.3125d)); // проверка, что найденная точка соответствует ожиданию
                 }
 
                 ++iteration;
@@ -196,22 +189,22 @@ namespace Tests
                 List<ParameterValue> calculationParameters = goldenSection.MakeCalculationParameters(parameters, variableParameterId, i);
                 if (i == 2)
                 {
-                    throughputCharacteristicValue = 0;
-                    Assert.That(Math.Round(goldenSection.X1), Is.EqualTo(20));// проверка, что точка X1 поменяла значение по формуле Фибоначчи
-                    _ = goldenSection.MoveBorder(throughputCharacteristicValue, i);
-                    Assert.That(goldenSection.NextPoint, Is.EqualTo("X1")); // проверка, что если точка "плохая", то снова будем искать X1
+                    Assert.That(Math.Round(goldenSection.X1), Is.EqualTo(20));// проверка, что точка X1 корректно рассчитана
+                    _ = goldenSection.MoveBorder(0, i);
+                    Assert.That(goldenSection.NextPoint, Is.EqualTo("X1")); // проверка, что снова будем искать X1
                 }
                 if (i == 3)
                 {
                     ParameterValue variableParameter = ParameterValue.GetFromListById(calculationParameters, variableParameterId);
                     throughputCharacteristicValue = ((DoubleParameterValue)variableParameter).Value;
-                    Assert.That(Math.Round(goldenSection.LastValue), Is.EqualTo(20)); // проверка, что правая граница поменялась на значение точки X1
+                    Assert.That(Math.Round(goldenSection.LastValue), Is.EqualTo(20)); // проверка, что правая граница
+                                                                                      // поменяла значение на значение точки X1
                     _ = goldenSection.MoveBorder(throughputCharacteristicValue, i);
-                    Assert.That(goldenSection.NextPoint, Is.EqualTo("X2")); // проверка, что точка "хорошая" и далее будем менять X2
+                    Assert.That(goldenSection.NextPoint, Is.EqualTo("X2")); // проверка, что далее будем искать X2
                 }
                 if (i == 4)
                 {
-                    Assert.That(Math.Round(goldenSection.X2), Is.EqualTo(13));// проверка, что точка X2 поменяла значение по формуле Фибоначчи
+                    Assert.That(Math.Round(goldenSection.X2), Is.EqualTo(13));// проверка, что точка X2 корректно рассчитана
                 }         
             }
         }
